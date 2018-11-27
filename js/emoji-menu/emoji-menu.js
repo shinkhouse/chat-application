c398ccdd4c86e3cf890a0bdcb2690c2fb13b7a3f
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2018-11-27 12:13:25 -0600

    add tooltips to emojis

--- js/emoji-menu/emoji-menu.js
+++ js/emoji-menu/emoji-menu.js
@@ -34,37 +34,48 @@ function changeMenu(button) {
         }
     }
 }
+
+function underscoreToSpaceDelimiter(string) {
+    var oldPhrase = string.split('_');
+    var newPhraseArray = [];
+    oldPhrase.forEach(function(word) {
+        newPhraseArray.push(word.charAt(0).toUpperCase() + word.slice(1));
+    });
+
+    var phrase = newPhraseArray.join(' ');
+    return phrase.charAt(0).toUpperCase() + phrase.slice(1);
+}
 function startup() {
     for (var i in activities.activity) {
-        $('.emoji-menu #activities-menu').append($('<div class="emoji">').text(activities.activity[i].value));
+        $('.emoji-menu #activities-menu').append($('<div class="emoji" title="' + underscoreToSpaceDelimiter(activities.activity[i].key) +'">').text(activities.activity[i].value));
     }
 
     for (var i in flags.flag) {
-        $('.emoji-menu #flags-menu').append($('<div class="emoji">').text(flags.flag[i].value));
+        $('.emoji-menu #flags-menu').append($('<div class="emoji" title="' + underscoreToSpaceDelimiter(flags.flag[i].key) +'">').text(flags.flag[i].value));
     }
 
     for (var i in foods.food) {
-        $('.emoji-menu #foods-menu').append($('<div class="emoji">').text(foods.food[i].value));
+        $('.emoji-menu #foods-menu').append($('<div class="emoji" title="' + underscoreToSpaceDelimiter(foods.food[i].key) +'">').text(foods.food[i].value));
     }
 
     for (var i in natures.nature) {
-        $('.emoji-menu #natures-menu').append($('<div class="emoji">').text(natures.nature[i].value));
+        $('.emoji-menu #natures-menu').append($('<div class="emoji" title="' + underscoreToSpaceDelimiter(natures.nature[i].key) +'">').text(natures.nature[i].value));
     }
 
     for (var i in objects.object) {
-        $('.emoji-menu #objects-menu').append($('<div class="emoji">').text(objects.object[i].value));
+        $('.emoji-menu #objects-menu').append($('<div class="emoji" title="' + underscoreToSpaceDelimiter(objects.object[i].key) +'">').text(objects.object[i].value));
     }
 
     for (var i in peoples.people) {
-        $('.emoji-menu #peoples-menu').append($('<div class="emoji">').text(peoples.people[i].value));
+        $('.emoji-menu #peoples-menu').append($('<div class="emoji" title="' + underscoreToSpaceDelimiter(peoples.people[i].key) +'">').text(peoples.people[i].value));
     }
 
     for (var i in symbols.symbol) {
-        $('.emoji-menu #symbols-menu').append($('<div class="emoji">').text(symbols.symbol[i].value));
+        $('.emoji-menu #symbols-menu').append($('<div class="emoji" title="' + underscoreToSpaceDelimiter(symbols.symbol[i].key) +'">').text(symbols.symbol[i].value));
     }
 
     for (var i in travels.travel) {
-        $('.emoji-menu #travels-menu').append($('<div class="emoji">').text(travels.travel[i].value));
+        $('.emoji-menu #travels-menu').append($('<div class="emoji" title="' + travels.travel[i].value +'">').text(travels.travel[i].value));
     }
 
     $('.emoji-menu .emoji').click(function () {
